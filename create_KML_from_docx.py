32b10646a4373ec667574dde220ee92961c15638
rahmansunbeam <blu_sr@yahoo.com>
2017-04-25 12:30:55 +0600

    Create create_KML_from_docx.py

---
+++ create_KML_from_docx.py
@@ -0,0 +1,134 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# from docx import Document
+# import re
+#
+# main_file = Document("D:/DOCUMENTS/Google_Link/1  Category I/1  Category I.docx")
+# table = main_file.tables[1]
+#
+# def extract_table():
+#     data = []
+#     keys = None
+#
+#     for i, row in enumerate(table.rows):
+#         text = (cell.text for cell in row.cells)
+#         if i == 0:
+#             keys = tuple(text)
+#             continue
+#
+#         row_data = tuple(text)
+#         data.append(row_data)
+#     return data
+#
+# def extract_text():
+#     regex_reference = re.compile("(C.-)\w+")
+#     col_reference = [item for item in extract_table() if regex_reference.match(item[1])]
+#     return col_reference
+#
+# # print len(extract_text())
+# for i in extract_text():
+#     print i
+
+#--------------------------------------------------------#
+# Wrap each files to its own folder
+# import os
+# import shutil
+#
+# dir_name = "D:\\DOCUMENTS\\Google_Link\\all docx"
+# docx_files = os.listdir(dir_name)
+#
+# for i in docx_files:
+#   os.mkdir(os.path.join(dir_name , i.split(".")[0]))
+#   shutil.move(os.path.join(dir_name , i), os.path.join(dir_name , i.split(".")[0]))
+#--------------------------------------------------------#
+from docx import Document
+import sys, os, re
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+root_dir = "C:\\Google_Link"
+
+for root, dirs, files in os.walk(root_dir):
+    for name in files:
+        doc_file = os.path.join(root, name)
+        if doc_file.endswith('docx'):
+            main_file = Document(doc_file)
+            table = main_file.tables[1]  # this is same for every document
+
+            data = []
+            keys = None
+
+            for i, row in enumerate(table.rows):
+                text = (cell.text for cell in row.cells)
+
+                if i == 0:
+                    keys = tuple(text)
+                    continue
+
+                row_data = tuple(text)
+                data.append(row_data)
+
+            regexReference = re.compile("(C?..-[0-9-]+)")
+            regexCoordinate = re.compile(r'(N-(.{,12})([0-9]|\')|[0-9].{,12}N)[;, ]+(E-(.{,12})([0-9]|\')|[0-9].{,12}E)')
+
+            result = []
+            for item in data:
+                tmp = dict()
+                matchReference = regexReference.search(item[1])
+                matchCoordinate = regexCoordinate.search(unicode(item[2]))
+                if matchReference:
+                    tmp['reference'] = matchReference.group()
+                if matchCoordinate:
+                    tmp['y'] = matchCoordinate.group(1)
+                    tmp['x'] = matchCoordinate.group(4)
+                tmp['location'] = unicode(item[3])
+                tmp['description'] = unicode(item[2])
+                result.append(tmp)
+
+            for rs in result:
+                if 'reference' in rs:
+                    # for k, v in rs.iteritems():
+                        # print('{} = {}'.format(k, v))
+                    # print rs['description']
+                    # print ">>>> Processed doc: " + doc_file
+
+                    kml_file_path = os.path.dirname(doc_file)
+
+                    with open(os.path.join(kml_file_path, rs['reference']+'.kml'), 'w') as f:
+                       # Writing the kml file.
+                       f.write("<?xml version='1.0' encoding='UTF-8'?>\n")
+                       f.write("<kml xmlns='http://earth.google.com/kml/2.1'>\n")
+                       f.write("<Document>\n")
+                       f.write("   <name>" + rs['reference'] + '.kml' + "</name>\n")
+                       f.write("   <Placemark>\n")
+                       f.write("       <name>" + rs['reference'] + "</name>\n")
+                       f.write("       <description>" + "\n \n" + rs['description'] + "</description>\n")
+                       f.write("       <Point>\n")
+                       f.write("           <coordinates>" + rs['x'] + "," + rs['y']  + "</coordinates>\n")
+                       f.write("       </Point>\n")
+                       f.write("   </Placemark>\n")
+                       f.write("</Document>\n")
+                       f.write("</kml>\n")
+                       f.close()
+
+#--------------------------------------------------------#
+
+# for rs in extractTableRow():
+#     if 'reference' in rs:
+        # print "(" + rs['x'] + ")(" + rs['y'] + ")"
+        # print rs['y']
+        # rx = re.compile(r'((?P<degree>\d+)°\s*(?P<minute>[^\'´]+)[\'´]?\b(?P<second>[^\"´´]+))')        #
+        # def convert(match):
+        #     try:
+        #         degree = float(match.group('degree'))
+        #         minute = float(match.group('minute'))
+        #         second = float(match.group('second'))
+        #         dd = degree + minute / 60
+        #     except:
+        #         dd = -1
+        #     finally:
+        #         return dd
+
+        # coordinates_long = [convert(match) for match in rx.finditer(rs['x'])]
+        # coordinates_lat = [convert(match) for match in rx.finditer(rs['y'])]
